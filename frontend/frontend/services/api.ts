72db79c3c38d601594d081f2f1f02357bd0a9aa9
codewithsjc <codewithsjc@gmail.com>
2026-01-04 21:46:44 +0530

    Create api.ts

---
+++ frontend/frontend/services/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+
+const API_URL = process.env.EXPO_PUBLIC_API_URL + "/api";
+
+export const api = axios.create({
+  baseURL: API_URL,
+  timeout: 15000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Attach token automatically
+api.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync("access_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
